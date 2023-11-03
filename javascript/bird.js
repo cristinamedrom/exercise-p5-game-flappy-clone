5dba1322bbea0dd4ee27d39f1e5fe0d5cc7e57ec
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-03 20:55:30 +0100

    clase player

---
+++ javascript/bird.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor(x, y, baseImg, flyImg, vel, grav) {
+        this.x = x;
+        this.y = y;
+        this.baseImg = baseImg;
+        this.flyImg = flyImg;
+        this.vel = vel;
+        this.grav = grav;
+    }
+}
--- javascript/p5.js
+++ javascript/p5.js
@@ -3,5 +3,5 @@ function setup() {
   }
   
   function draw() {
-    background('#ffaaee');
+    background('#66bdc6');
   }
