aea8be34eb69d8831d33e9426b760630c1783e74
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-04 13:03:20 +0100

    establecer fondo

--- javascript/p5.js
+++ javascript/p5.js
@@ -1,18 +1,21 @@
 let bird;
+let fondo;
 
 function preload() {
   baseImg = loadImage('./imgs/redbird-midflap.png');
-  flyImg = loadImage('./imgs/redbird-upflap.png')
+  flyImg = loadImage('./imgs/redbird-upflap.png');
+  fondo = loadImage('./imgs/background-day.png');
+
 }
 
 function setup() {
-  bird = new Player(200, 325, baseImg, flyImg, 1, 3);
-  createCanvas(400, 650);
+  bird = new Player(127, 244, baseImg, flyImg, 1, 3);
+  createCanvas(288, 512);
   
   
 }
 
 function draw() {
-  background('#66bdc6');
+  background(fondo);
   bird.draw();
 }
