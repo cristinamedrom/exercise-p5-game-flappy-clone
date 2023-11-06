4f6c6cd95893098328a25ab32c9da0ad0241e4fb
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-06 17:06:07 +0100

    cambios

--- javascript/p5.js
+++ javascript/p5.js
@@ -1,18 +1,23 @@
 let bird;
 let fondo;
 let sueloBase;
+let tuberiaVerde;
 
 function preload() {
   baseImg = loadImage('./imgs/redbird-midflap.png');
   flyImg = loadImage('./imgs/redbird-upflap.png');
   fondo = loadImage('./imgs/background-day.png');
-  sueloImg = loadImage('./imgs/suelo.png')
+  sueloImg = loadImage('./imgs/suelo.png');
+  tuberiaImg = loadImage('./imgs/pipe-green.png');
+  tuberiaImgSup = loadImage('./imgs/pipe-green-180.png');
 
 }
 
 function setup() {
   bird = new Player(127, 244, baseImg, flyImg, 5, 1);
   sueloGame = new Suelo(0, 400, sueloImg);
+  tuberiaSuelo = new TuberiaInf(200, 350, tuberiaImg);
+  /*tuberiaCielo = new TuberiaSup(200, 150, tuberiaImg Sup);*/
   createCanvas(288, 512);
   
   
@@ -22,6 +27,10 @@ function draw() {
   background(fondo);
   bird.update();
   bird.draw();
+  tuberiaSuelo.draw();
+  /*tuberiaCielo.draw();*/
   sueloGame.draw();
+
+
 }
 
--- javascript/pipe.js
+++ javascript/pipe.js
@@ -5,7 +5,40 @@ class Suelo {
         this.sueloImg = sueloImg;
     }
 
+    movSuelo(sueloGame) {
+
+    }
+
     draw() {
         image(this.sueloImg, this.x, this.y);
+    }
+}
+
+class TuberiaInf {
+    constructor(x, y, tuberiaImg,) {
+        this.x = x;
+        this.y = y;
+        this.tuberiaImg = tuberiaImg;
+        this.velX = random(2, 4);
+    }
+
+    move() {
+        this.x += this.velX;
+    }
+
+    draw() {
+        image(this.tuberiaImg, this.x, this.y,)
+    }
+}
+
+class TuberiaSup {
+    constructor(x, y, tuberiaImgSup,) {
+        this.x = x;
+        this.y = y;
+        this.tuberiaImg = tuberiaImgSup;
+    }
+
+    draw() {
+        image(this.tuberiaImgSup, this.x, this.y,)
     }
 }
